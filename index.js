60e53f3cb6455214312c9dfe9af66351fa589559
Gokul S <gokul270520@gmail.com>
2022-04-08 14:34:06 +0530

    changepond website

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("part-two").style.backgroundColor="#1089FF";
+
+
+document.getElementById("part-five").style.backgroundColor="#ECECEC";
+document.getElementById("part-nine").style.backgroundColor="black";
